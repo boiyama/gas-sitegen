61eb2abea1af8092e3416cb8d765199f70038ea5
boiyaa <boiyaa@hotmail.com>
2019-02-21 21:12:06 +0900

    Add sheet tab

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,21 @@ declare var OAuth2: any;
 declare var process: any;
 
 global.build = () => {
+  // List sheet names
+  const sheets = SpreadsheetApp.openById(process.env.DRIVE_FILE_ID).getSheets();
+  const sheetNames = sheets.map(sheet => sheet.getName());
+  Logger.log(sheetNames);
+
+  // Export sheet HTML
   DriveApp.getStorageUsed();
-
   const blob = exportSheetAsZippedHTML(process.env.DRIVE_FILE_ID);
-
   const blobs = Utilities.unzip((blob as unknown) as GoogleAppsScript.Base.BlobSource);
 
-  deploy(blobs);
+  // Insert sheet tab into sheets
+  const newBlobs = blobs.map(insertSheetTab(sheetNames));
+
+  // Deploy
+  deploy(newBlobs);
 };
 
 const exportSheetAsZippedHTML = fileId =>
@@ -17,6 +25,40 @@ const exportSheetAsZippedHTML = fileId =>
     method: "get",
     headers: { Authorization: `Bearer ${ScriptApp.getOAuthToken()}` }
   }).getBlob();
+
+const insertSheetTab = sheetNames => blob => {
+  if (blob.getName() === "resources/sheet.css") {
+    return blob;
+  }
+
+  const html =
+    '<div id="docs-editor">' +
+    '<div id="waffle-grid-container" style="box-sizing: border-box; height: 100%; padding-bottom: 40px;">' +
+    // Sheet HTML
+    blob.getDataAsString() +
+    "</div>" +
+    // Create sheet tab HTML
+    '<style>.docs-sheet-tab{height: 100%;padding: 8px 16px 0;}</style><table id="grid-bottom-bar" style="position: fixed; bottom: 0px; z-index: 1000;" class="grid-bottom-bar " cellspacing="0" cellpadding="0" dir="ltr" role="presentation"><tbody><tr><td style="width:120px;">&nbsp;</td><td class="docs-sheet-outer-container"><div class="goog-inline-block"><div class="docs-sheet-container goog-inline-block" style="height: 39px;"><div class="docs-sheet-container-bar goog-toolbar goog-inline-block" role="toolbar" style="user-select: none;">' +
+    sheetNames.reduce((content, sheetName) => {
+      const active = blob.getName().replace(".html", "") === sheetName ? " docs-sheet-active-tab" : "";
+      return (
+        content +
+        '<a href="' +
+        sheetName +
+        '.html" class="goog-inline-block docs-sheet-tab docs-material' +
+        active +
+        '" role="button" aria-expanded="false" tabindex="0" aria-haspopup="true" id=":6qd"><div class="goog-inline-block docs-sheet-tab-outer-box"><div class="goog-inline-block docs-sheet-tab-inner-box"><div class="goog-inline-block docs-sheet-tab-caption"><span dir="ltr" class="docs-sheet-tab-name" spellcheck="false">' +
+        sheetName +
+        "</span></div></div></div></a>"
+      );
+    }, "") +
+    "</div></div></div></td></tr></tbody></table>" +
+    "</div>";
+
+  blob.setDataFromString(html);
+
+  return blob;
+};
 
 const firebaseKey = JSON.parse(process.env.FIREBASE_KEY);
 
@@ -36,7 +78,8 @@ const deploy = (blobs: GoogleAppsScript.Base.Blob[]) => {
       headers: { Authorization: `Bearer ${accessToken}` }
     }).getContentText()
   );
-  console.log("Version was created.", version);
+  Logger.log("Version was created.");
+  Logger.log(version);
 
   const fileInfos = blobs.map(blob => {
     const gzipBlob = Utilities.gzip((blob as unknown) as GoogleAppsScript.Base.BlobSource);
@@ -63,7 +106,8 @@ const deploy = (blobs: GoogleAppsScript.Base.Blob[]) => {
       payload: JSON.stringify({ files })
     }).getContentText()
   );
-  console.log("Files were populated.", uploadInfo);
+  Logger.log("Files were populated.");
+  Logger.log(uploadInfo);
 
   uploadInfo.uploadRequiredHashes.forEach(uploadRequiredHash => {
     const fileInfo = fileInfos.filter(fileInfo => fileInfo.hash === uploadRequiredHash)[0];
@@ -74,7 +118,7 @@ const deploy = (blobs: GoogleAppsScript.Base.Blob[]) => {
       contentType: "application/octet-stream",
       payload: fileInfo.gzipBlob.getBytes()
     });
-    console.log(`${fileInfo.blobName} was uploaded.`);
+    Logger.log(`${fileInfo.blobName} was uploaded.`);
   });
 
   const finalizedVersion = JSON.parse(
@@ -85,7 +129,8 @@ const deploy = (blobs: GoogleAppsScript.Base.Blob[]) => {
       payload: JSON.stringify({ status: "FINALIZED" })
     }).getContentText()
   );
-  console.log("Version was finalized.", finalizedVersion);
+  Logger.log("Version was finalized.");
+  Logger.log(finalizedVersion);
 
   const release = JSON.parse(
     UrlFetchApp.fetch(`https://firebasehosting.googleapis.com/v1beta1/sites/${firebaseKey.project_id}/releases?versionName=${version.name}`, {
@@ -93,5 +138,6 @@ const deploy = (blobs: GoogleAppsScript.Base.Blob[]) => {
       headers: { Authorization: `Bearer ${accessToken}` }
     }).getContentText()
   );
-  console.log("Release was created.", release);
+  Logger.log("Release was created.");
+  Logger.log(release);
 };
